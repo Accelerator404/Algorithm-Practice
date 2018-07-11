3330af351fa22662506e605e1b57b4e35e4962de
Yuudachi_CivilEng <adsl2328606@gmail.com>
2018-07-11 21:22:05 +0800

    Create 1013.cpp
    When running at last checkpoint this code will be timeout.

---
+++ PAT_Advanced/1013.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+//PAT Advanced 1013 Battle Over Cities
+
+/*
+ * 题意说明，对N个城市现有M条开放的公路使所有城市互联，
+ * 我们需要考虑其中一个城市被占领后其余城市需要修复多少条公路才能使其他城市再次处于互联。
+ * 比如关闭城市1后剩余城市组成两个被分隔的内部互联子网，那么修复一条高速公路即可使其恢复互联。
+ * 简而言之，关闭城市K后，剩余城市组成P个内部互联子网，则修复P-1条高速公路。
+ * 可以考虑给所有的城市连接到一个DFS搜索的虚入口，从虚入口对每个城市进行一次DFS，由此判断子网个数。
+ */
+int map[1001][1001] = {0};
+bool visited[1001] = {false};
+int N;
+
+void DFS(int node){
+    if(visited[node])
+        return;
+    visited[node] = true;
+    for (int i = 1; i <= N; ++i) {
+        if(map[node][i] == 1){
+            DFS(i);
+        }
+    }
+}
+
+int main() {
+    int M,K;
+    cin >> N >> M >> K;
+    for (int i = 0; i < M; ++i) {
+        int cityA,cityB;
+        cin >> cityA >> cityB;
+        map[cityA][cityB] = map[cityB][cityA] = 1;
+    }
+    for (int j = 0; j < K; ++j) {
+        int occupiedCity,zone = 0;
+        cin >> occupiedCity;
+        fill(visited,visited+1001,false);
+        visited[occupiedCity] = true;
+        for (int i = 1; i <= N; ++i) {
+            if(!visited[i]){
+                DFS(i);
+                zone++;
+            }
+        }
+        cout << zone-1 << endl;
+    }
+    return 0;
+}
