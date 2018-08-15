4fe5546a7b2a11a8e40819279a6993d4aa074b24
Yuudachi_CivilEng <adsl2328606@gmail.com>
2018-08-16 03:17:12 +0800

    Update 1019.cpp

--- PAT_Advanced/1019.cpp
+++ PAT_Advanced/1019.cpp
@@ -5,21 +5,29 @@ using namespace std;
 
 //PAT Advanced 1019 General Palindromic Number
 
+/* 第一步，余数法
+ * 第二步，反转余数列
+ * 第三步，比较首尾
+ */
+
 int main(){
-    long long int N,b;
+    int N,b;
     cin >> N >> b;
-    long long int tempDigit = 0;
-    vector<long long int> result,reverseResult;
+    if(N == 0){
+        cout << "Yes" << endl << 0 << endl;
+        return 0;
+    }
+    int tempDigit = 0;
+    vector<int> result;
     while (N > 0){
         tempDigit = N % b;
         result.push_back(tempDigit);
         N /= b;
     }
-    reverseResult = result;
     reverse(result.begin(),result.end());
     bool isPalindromic = true;
-    for (int i = 0; i < result.size(); ++i) {
-        if(result[i] != reverseResult[result.size()-i]){
+    for (int i = 0; i < result.size()/2 + 1; ++i) {
+        if(result[i] != result[result.size()-1-i]){
             cout << "No" << endl;
             isPalindromic = false;
             break;
