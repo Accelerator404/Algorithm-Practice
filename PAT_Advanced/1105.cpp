5e8ff5dbadb6a152768af7b5c42e60bd5cdaba7f
Yuudachi_CivilEng <adsl2328606@gmail.com>
2018-08-22 17:58:11 +0800

    Update 1105.cpp

--- PAT_Advanced/1105.cpp
+++ PAT_Advanced/1105.cpp
@@ -6,28 +6,59 @@ using namespace std;
 
 //PAT Advanced 1105 Spiral Matrix
 
-int findM(int N){
-    int roof = ceil(sqrt(N))+1,m = N,n = 1;
-    for (int i = roof; i >= 1; --i) {
-        if(n > m)
-            break;
-        if(N % i == 0){
-            m = i;
-            n = N / m;
-        }
+int findN(int N){
+    int i = sqrt((double)N);
+    while(i >= 1) {
+        if(N % i == 0)
+            return i;
+        i--;
     }
-    return m;
+    return 1;
 }
 
 int main(){
-    int N,Matrix[10001][10001];
+    int N;
     cin >> N;
     vector<int> seq(N);
     for (int i = 0; i < N; ++i) {
-        cin >> seq[i];
+        scanf("%d",&seq[i]);
     }
     sort(seq.begin(),seq.end(),greater_equal<int>());
-    //todo
+    int n = findN(N);
+    int m = N/n;
+    vector<vector<int>> matrix(m,vector<int>(n));
+    //绕矩阵一圈为循环一次
+    int level = m / 2 + m % 2,t = 0;
+    for (int i = 0; i < level; ++i) {
+        for (int j = i; j <= n-1-i && t < N; ++j) {
+            matrix[i][j] = seq[t];
+            t++;
+        }
+        for (int j = i + 1; j <= m - 1 - i ; ++j) {
+            matrix[j][n - 1 - i] = seq[t];
+            t++;
+        }
+        for (int j = n - 2 - i; j >= i; --j) {
+            matrix[m - 1 -i][j] = seq[t];
+            t++;
+        }
+        for (int j = m - 2 -i; j > i; --j) {
+            matrix[j][i] = seq[t];
+            t++;
+        }
+    }
+    for (int i = 0; i < m; ++i) {
+        bool first =true;
+        for (int j = 0; j < n; ++j) {
+            if(first){
+                printf("%d",matrix[i][j]);
+                first = false;
+            }
+            else
+                printf(" %d",matrix[i][j]);
 
+        }
+        printf("\n");
+    }
     return 0;
 }
