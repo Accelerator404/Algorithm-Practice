77aae5abb1c6ab5f4b8a56403d813a1238f02c23
Yuudachi_CivilEng <adsl2328606@gmail.com>
2018-08-24 02:41:10 +0800

    Update 1106.cpp

--- PAT_Advanced/1106.cpp
+++ PAT_Advanced/1106.cpp
@@ -6,33 +6,28 @@ using namespace std;
 
 //PAT Advanced 1106 Lowest Price in Supply Chain
 
+/*
+ * 价格只与深度相关，统计深度即可，深度越小越便宜，在同一深度则价格相同。
+ */
+
 struct node{
     //retailer:character == 0; distributor:character == 1; root supplier:character == -1;
-    int character;
-    double price;
+    int character,depth;
     vector<int> subID;
 };
 
-double accuFormat(double input){
-    double u;
-    char str[16];
-    sprintf(str,"%.04f",input);
-    sscanf(str,"%lf",&u);
-    return u;
-}
-
-void traversal(int ID,vector<node> & chainList,const double & r){
+void traversal(int ID,vector<node> & chainList){
     if(chainList[ID].character != 0){
         for (int i = 0; i < chainList[ID].subID.size(); ++i) {
-            chainList[chainList[ID].subID[i]].price = accuFormat(chainList[ID].price * (1.0 + r * 0.01));
-            traversal(chainList[ID].subID[i],chainList,r);
+            chainList[chainList[ID].subID[i]].depth = chainList[ID].depth + 1;
+            traversal(chainList[ID].subID[i],chainList);
         }
     } else
         return;
 }
 
 bool compare(node retailerA,node retailerB){
-    return retailerA.price < retailerB.price;
+    return retailerA.depth < retailerB.depth;
 }
 
 int main(){
@@ -58,15 +53,15 @@ int main(){
         }
         chainList[i].character = (i == 0)?-1:1;
     }
-    chainList[0].price = P;
-    traversal(0,chainList,r);
+    chainList[0].depth = 0;
+    traversal(0,chainList);
     for (int i = 0; i < retailerIDList.size(); ++i)
         retailerList.push_back(chainList[retailerIDList[i]]);
     sort(retailerList.begin(),retailerList.end(),compare);
-    double minPrice = retailerList[0].price;
+    double minPrice = P * pow(1 + r/100.0,retailerList[0].depth);
     int count = 0;
     for (int i = 0; i < retailerList.size(); ++i) {
-        if(retailerList[i].price <= minPrice + 0.00009){
+        if(retailerList[i].depth == retailerList[0].depth){
             count++;
         } else break;
     }
