ad460064b4c0efdc55153af75d54244c736d3bd0
Yuudachi_CivilEng <adsl2328606@gmail.com>
2018-08-24 03:21:50 +0800

    Update 1046.cpp

--- PAT_Advanced/1046.cpp
+++ PAT_Advanced/1046.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 using namespace std;
 
 //PAT Advanced 1046 Shortest Distance
 
 int main(){
-    int N,M,circultL = 0;
-    vector<int> D;
+    int N,M,circuitL = 0;
+    vector<int> D,distFromE1;
     cin >> N;
-    D.resize(N);
+    D.resize(N+1),distFromE1.resize(N+1);
+    distFromE1[1] = 0;
     for (int i = 1; i <= N; ++i) {
         scanf("%d",&D[i]);
-        circultL += D[i];
+        if(i>1)
+            distFromE1[i] = distFromE1[i-1] + D[i-1];
+        circuitL += D[i];
     }
     cin >> M;
     while (M-- > 0){
@@ -24,11 +26,9 @@ int main(){
             Eb = Ea;
             Ea = tmp;
         }
-        for (int i = Ea; i < Eb; ++i) {
-            dist += D[i];
-        }
-        if(dist > circultL - dist)
-            printf("%d\n",circultL - dist);
+        dist = distFromE1[Eb] - distFromE1[Ea];
+        if(dist > circuitL - dist)
+            printf("%d\n",circuitL - dist);
         else
             printf("%d\n",dist);
     }
