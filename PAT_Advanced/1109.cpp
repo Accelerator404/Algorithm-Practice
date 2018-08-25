adf3222d53741e027f1f85e40202cdbcc7e4d347
Yuudachi_CivilEng <adsl2328606@gmail.com>
2018-08-25 21:05:39 +0800

    Update 1109.cpp

--- PAT_Advanced/1109.cpp
+++ PAT_Advanced/1109.cpp
@@ -8,7 +8,8 @@ using namespace std;
 //PAT Advanced 1109 Group Photo
 
 
-/* 注意，该题未AC。
+/*
+ * 不知怎么的，就在改每一行输出范围的过程中取得AC了。
  */
 struct student{
     string name;
@@ -16,13 +17,6 @@ struct student{
 };
 
 bool compare(student A,student B){
-    if(A.height != B.height)
-        return A.height < B.height;
-    else
-        return A.name < B.name;
-}
-
-bool compare2(student A,student B){
     if(A.height != B.height)
         return A.height > B.height;
     else
@@ -32,45 +26,38 @@ bool compare2(student A,student B){
 int main(){
     int N,K,rowN,rear;
     cin >> N >> K;
-    cin.sync_with_stdio(false);
+    //cin.sync_with_stdio(false);
     rowN = (int)floor(N/K);
-    if(rowN * K < N || rowN * K > N)
-        rear = N - rowN * (K - 1);
-    else
-        rear = rowN;
-    vector<vector<student>> M(K,vector<student>(rowN + 2));
+    rear = N - rowN * (K - 1);
     vector<student> inbox(N);
-    M[K-1].resize(rear + 2);
     for (int i = 0; i < N; ++i) {
         cin >> inbox[i].name >> inbox[i].height;
     }
     sort(inbox.begin(),inbox.end(),compare);
     for (int i = 0,p = 0; i < K ; ++i) {
-        int rSize = (i == K-1)?rear:rowN;
-        vector<student> thisRow(rSize);
-        for (int j = 0; j < rSize; ++j,++p) {
+        int rSize = (i == 0)?rear:rowN;
+        vector<student> thisRow(rSize+1),sorted(rSize+1);
+        for (int j = 1; j < rSize+1; ++j,++p) {
             thisRow[j] = inbox[p];
         }
-        sort(thisRow.begin(),thisRow.end(),compare2);
         int mid = (int)floor(rSize/2+1);
-        M[i][mid] = thisRow[0];
-        for (int k = 1;; ++k) {
-            int pL = mid - k,pR = mid + k;
-            if(pL <= 0)
+        int ptr = 1;
+        sorted[mid] = thisRow[ptr];
+        for (int k = 1;ptr <= rSize; ++k) {
+            ptr++;
+            if(ptr > rSize)
                 break;
-            M[i][pL] = thisRow[2*k-1];
-            if(pR >= rSize + 1)
+            sorted[mid - k] = thisRow[ptr];
+            ptr++;
+            if(ptr > rSize)
                 break;
-            M[i][pR] = thisRow[2*k];
+            sorted[mid + k] = thisRow[ptr];
         }
-    }
-    for (int i = K-1; i >= 0; --i) {
-        int rSize = (i == K-1)?rear:rowN;
-        for (int j = 1; j <= rSize; ++j) {
+        for (int j = 1; j < rSize+1; ++j) {
             if(j == 1)
-                printf("%s",M[i][j].name.c_str());
+                printf("%s",sorted[j].name.c_str());
             else
-                printf(" %s",M[i][j].name.c_str());
+                printf(" %s",sorted[j].name.c_str());
         }
         printf("\n");
     }
