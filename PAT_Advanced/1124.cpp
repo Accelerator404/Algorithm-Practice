98820fc85aab045a049c2479ad7942166a8d7a44
Yuudachi_CivilEng <adsl2328606@gmail.com>
2018-09-06 23:04:51 +0800

    Create 1124.cpp

---
+++ PAT_Advanced/1124.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+//PAT Advanced Level 1124 Raffle for Weibo Followers
+
+int main() {
+    int M,N,S;
+    cin >> M >> N >> S;
+    vector<string> follower(M+1),winner;
+    for (int i = 1; i < M+1; ++i) {
+        cin >> follower[i];
+    }
+    for (int i = 0; i * N + S <= M; ++i) {
+        if(find(winner.begin(),winner.end(),follower[S+i*N]) == winner.end())
+            winner.push_back(follower[S+i*N]);
+        else{
+            int ptr = S+i*N;
+            while (find(winner.begin(),winner.end(),follower[ptr]) != winner.end())
+                ptr++;
+            winner.push_back(follower[ptr]);
+        }
+    }
+    if(winner.empty())
+        cout << "Keep going..." << endl;
+    else
+        for (int i = 0; i < winner.size(); ++i) {
+            cout << winner[i] << endl;
+        }
+    return 0;
+}
