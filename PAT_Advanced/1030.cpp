61bd2e80358f075475b67ea62d388446b295782c
Yuudachi_CivilEng <adsl2328606@gmail.com>
2018-09-07 18:01:46 +0800

    Update 1030.cpp
    将DFS过程简化，计算比较路费的过程放入Dijkstra算法中

--- PAT_Advanced/1030.cpp
+++ PAT_Advanced/1030.cpp
@@ -8,38 +8,22 @@ using namespace std;
 
 const int INF = 99999999;
 //e、cost是邻接矩阵，分别记录城市间距离和路费，dis存储Dijkstra算法中的v点到起点最短路径距离
-int route[505][505],dist[505],cost[505][505];
+int route[505][505],dist[505],cost[505][505],costWeight[505];
 int N,M,S,D;
 //pre数组指每一点v到出发点的所有有“最短距离dis”的路径中，到v点的前一点
-vector<int> previous[505];
+vector<int> previous(505);
 vector<int> tempPath,FinalPath;
 int minCost = INF;
 
 void dfs(int v) {
     //将当前点压入DFS的临时路径
-    tempPath.push_back(v);
+    FinalPath.push_back(v);
     //到达起点便开始判断路费（Dijkstra算法已确定所有路径都是最短路径）
     if(v == S){
-        int tempCost = 0;
-        //计算路费
-        for(int i = tempPath.size() - 1;i > 0;i--){
-            int thisCity = tempPath[i],nextCity = tempPath[i-1];
-            tempCost += cost[thisCity][nextCity];
-        }
-        //若路费比当前最小更小，更新最优路径
-        if(tempCost < minCost){
-            minCost = tempCost;
-            FinalPath = tempPath;
-        }
-        //弹出临时路径当前节点，返回void
-        tempPath.pop_back();
         return;
     }
     //未到起点，则压入所有pre数组中的“前一点”
-    for(int i = 0; i < previous[v].size();i++)
-        dfs(previous[v][i]);
-    //弹出临时路径中当前点，便于上一节点搜索其他的“前一点”
-    tempPath.pop_back();
+    dfs(previous[v]);
 }
 
 int main() {
@@ -47,6 +31,7 @@ int main() {
     bool isVisited[505];
     fill(route[0],route[0]+505*505,INF);
     fill(dist,dist+505,INF);
+    fill(costWeight,costWeight+505,0);
     fill(isVisited,isVisited+505,false);
     cin >> N >> M >> S >> D;
     //读入邻接矩阵
@@ -59,7 +44,7 @@ int main() {
     }
     //初始化起点
     dist[S] = 0;
-    previous[S].push_back(S);
+    previous[S] = S;
     //遍历所有节点
     for(int i = 0; i < N; i++) {
         int minD = INF,u = -1;
@@ -77,20 +62,23 @@ int main() {
         for(int v = 0; v < N; v++) {
             //若v点未访问且uv相通
             if(!isVisited[v] && route[u][v] < INF) {
-                //若通过u点，距离更小，则更新距离，删除所有前节点，压入当前节点
+                //若通过u点，距离更小，则更新距离，更新前节点为当前节点
                 if(dist[v] > dist[u] + route[u][v]){
                     dist[v] = dist[u] + route[u][v];
-                    previous[v].clear();
-                    previous[v].push_back(u);
+                    previous[v] = u;
+                    costWeight[v] = costWeight[u] + cost[u][v];
                 }
-                //若两条路径等长，压入当前节点
-                else if(dist[v] == dist[u] + route[u][v])
-                    previous[v].push_back(u);
+                //若两条路径等长时走当前节点路费更省，更新前节点为当前节点
+                else if(dist[v] == dist[u] + route[u][v] && costWeight[v] > costWeight[u] + cost[u][v]) {
+                    previous[v] = u;
+                    costWeight[v] = costWeight[u] + cost[u][v];
+                }
             }
         }
     }
     //从终点开始DFS
     dfs(D);
+    minCost = costWeight[D];
     for(int i = FinalPath.size()-1;i >= 0;i--){
         printf("%d ",FinalPath[i]);
     }
