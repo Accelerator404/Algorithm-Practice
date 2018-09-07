813a0cf6fc222a34d677f1810813b813b790515f
Yuudachi_CivilEng <adsl2328606@gmail.com>
2018-09-07 17:41:02 +0800

    Update 1111.cpp

--- PAT_Advanced/1111.cpp
+++ PAT_Advanced/1111.cpp
@@ -5,149 +5,122 @@ using namespace std;
 
 //PAT Advanced Level 1111 Online Map
 
-const int INF = INT32_MAX;
-vector<int> tempPath,shortPath,fastPath;
-int minTime = INF;
+/* 两次Dijkstra
+ */
 
-void dfs(int v,int S,vector<vector<int>> & pre,vector<vector<int>> & time){
-    tempPath.push_back(v);
-    if(v == S){
-        int tempTime = 0;
-        for(int i = tempPath.size() - 1;i > 0;i--){
-            int thisCity = tempPath[i],nextCity = tempPath[i-1];
-            tempTime += time[thisCity][nextCity];
-        }
-        if(tempTime < minTime){
-            minTime = tempTime;
-            shortPath = tempPath;
-        }
-        tempPath.pop_back();
-        return;
-    }
-    for(int i = 0; i < pre[v].size();i++)
-        dfs(pre[v][i],S,pre,time);
-    tempPath.pop_back();
+const int inf = 999999999;
+int dis[510], Time[510], e[510][510], w[510][510], dispre[510],Timepre[510], weight[510],NodeNum[510];
+bool visit[510];
+vector<int> dispath, Timepath, temppath;
+int S, D, minnode = inf;
+void dfsdispath(int v) {
+    dispath.push_back(v);
+    if(v == S) return ;
+    dfsdispath(dispre[v]);
 }
-
-void dfs2(int v,int S,vector<vector<int>> & pre){
-    tempPath.push_back(v);
-    if(v == S){
-        fastPath = tempPath;
-        tempPath.pop_back();
-        return;
-    }
-    for(int i = 0; i < pre[v].size();i++)
-        dfs2(pre[v][i],S,pre);
-    tempPath.pop_back();
+void dfsTimepath(int v) {
+    Timepath.push_back(v);
+    if(v == S) return ;
+    dfsTimepath(Timepre[v]);
 }
-
-
 int main() {
-    int N,M,S,D;
-    cin >> N >> M;
-    vector<vector<int>> length(N,vector<int>(N,0));
-    vector<vector<int>> time(N,vector<int>(N,0));
-    for (int i = 0; i < M; ++i) {
-        int v1,v2,ow,len,t;
-        scanf("%d%d%d%d%d",&v1,&v2,&ow,&len,&t);
-        length[v1][v2] = len;
-        time[v1][v2] = t;
-        if(ow == 0){
-            length[v2][v1] = len;
-            time[v2][v1] = t;
+    fill(dis, dis + 510, inf);
+    fill(Time, Time + 510, inf);
+    fill(weight, weight + 510, inf);
+    fill(e[0], e[0] + 510 * 510, inf);
+    fill(w[0], w[0] + 510 * 510, inf);
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int a, b, flag, len, t;
+    for(int i = 0; i < m; i++) {
+        scanf("%d %d %d %d %d", &a, &b, &flag, &len, &t);
+        e[a][b] = len;
+        w[a][b] = t;
+        if(flag != 1) {
+            e[b][a] = len;
+            w[b][a] = t;
         }
     }
-    cin >> S >> D;
-    //以下是最短路线计算
-    vector<int> dist(N,INF);
-    bool visited[510];
-    vector<vector<int>> pre(510);
-    dist[S] = 0;
-    pre[S].push_back(S);
-    for (int i = 0; i < N; ++i) {
-        int minDist = INF,u = -1;
-        for (int j = 0; j < N; ++j) {
-            if(!visited[j] && dist[j] < minDist){
+    scanf("%d %d", &S, &D);
+    dis[S] = 0;
+    for(int i = 0; i < n; i++)
+        dispre[i] = i;
+    for(int i = 0; i < n; i++) {
+        int u = -1, minn = inf;
+        for(int j = 0; j < n; j++) {
+            if(visit[j] == false && dis[j] < minn) {
                 u = j;
-                minDist = dist[j];
+                minn = dis[j];
             }
         }
-        if(u == -1)
-            break;
-        visited[u] = true;
-        for (int v = 0; v < N; ++v) {
-            if(!visited[v] && length[u][v] < INF){
-                if(dist[v] > dist[u] + length[u][v]){
-                    dist[v] = dist[u] + length[u][v];
-                    pre[v].clear();
-                    pre[v].push_back(u);
-                } else if(dist[v] == dist[u] + length[u][v]){
-                    pre[v].push_back(u);
+        if(u == -1) break;
+        visit[u] = true;
+        for(int v = 0; v < n; v++) {
+            if(visit[v] == false && e[u][v] != inf) {
+                if(e[u][v] + dis[u] < dis[v]) {
+                    dis[v] = e[u][v] + dis[u];
+                    dispre[v] = u;
+                    weight[v] = weight[u] + w[u][v];
+                } else if(e[u][v] + dis[u] == dis[v] && weight[v] > weight[u] + w[u][v]) {
+                    weight[v] = weight[u] + w[u][v];
+                    dispre[v] = u;
                 }
             }
         }
     }
-    dfs(D,S,pre,time);
-    tempPath.clear();
-
-    //以下是最快路线计算
-    vector<int> totalTime(N,INF);
-    bool visited2[510];
-    vector<vector<int>> pre2(510);
-    totalTime[S] = 0;
-    pre2[S].push_back(S);
-    for (int i = 0; i < N; ++i) {
-        int minTime = INF,u = -1;
-        for (int j = 0; j < N; ++j) {
-            if(!visited2[j] && totalTime[j] < minTime){
+    dfsdispath(D);
+    Time[S] = 0;
+    fill(visit, visit + 510, false);
+    for(int i = 0; i < n; i++) {
+        int u = -1, minn = inf;
+        for(int j = 0; j < n; j++) {
+            if(visit[j] == false && minn > Time[j]) {
                 u = j;
-                minTime = dist[j];
+                minn = Time[j];
             }
         }
-        if(u == -1)
-            break;
-        visited2[u] = true;
-        for (int v = 0; v < N; ++v) {
-            if(!visited2[v] && length[u][v] < INF){
-                if(totalTime[v] > totalTime[u] + time[u][v]){
-                    totalTime[v] = totalTime[u] + time[u][v];
-                    pre2[v].clear();
-                    pre2[v].push_back(u);
-                } else if(totalTime[v] == totalTime[u] + time[u][v]){
-                    pre2[v].push_back(u);
+        if(u == -1) break;
+        visit[u] = true;
+        for(int v = 0; v < n; v++) {
+            if(visit[v] == false && w[u][v] != inf) {
+                if(w[u][v] + Time[u] < Time[v]) {
+                    Time[v] = w[u][v] + Time[u];
+                    Timepre[v]=(u);
+                    NodeNum[v]=NodeNum[u]+1;
+                } else if(w[u][v] + Time[u] == Time[v]&&NodeNum[u]+1<NodeNum[v]) {
+                    Timepre[v]=(u);
+                    NodeNum[v]=NodeNum[u]+1;
                 }
             }
         }
     }
-    dfs2(D,S,pre2);
-    reverse(shortPath.begin(),shortPath.end());
-    reverse(fastPath.begin(),fastPath.end());
-    vector<int> shortPath1 = shortPath,fastPath2 = fastPath;
-    if(shortPath1 == fastPath2){
-        cout << "Distance = " << dist[D] <<"; Time = " <<totalTime[D];
+    dfsTimepath(D);
+    reverse(dispath.begin(),dispath.end());
+    reverse(Timepath.begin(),Timepath.end());
+    if(dispath == Timepath){
+        cout << "Distance = " << dis[D] <<"; Time = " <<Time[D];
         cout << ": ";
-        for (int i = 0; i < shortPath1.size(); ++i) {
+        for (int i = 0; i < dispath.size(); ++i) {
             if(i>0)
                 cout  << " -> ";
-            cout << shortPath1[i];
+            cout << dispath[i];
         }
     }
     else{
-        cout << "Distance = " << dist[D] <<": ";
-        for (int i = 0; i < shortPath1.size(); ++i) {
+        cout << "Distance = " << dis[D] <<": ";
+        for (int i = 0; i < dispath.size(); ++i) {
             if(i>0)
                 cout  << " -> ";
-            cout << shortPath1[i];
+            cout << dispath[i];
         }
         cout << endl;
-        cout << "Time = " << totalTime[D] <<": ";
-        for (int i = 0; i < fastPath2.size(); ++i) {
+        cout << "Time = " << Time[D] <<": ";
+        for (int i = 0; i < Timepath.size(); ++i) {
             if(i>0)
                 cout  << " -> ";
-            cout << fastPath2[i];
+            cout << Timepath[i];
         }
         cout << endl;
-
     }
 
     return 0;
