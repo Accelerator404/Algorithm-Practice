21cdaf2b209cdd9b7e9361e09691df24c79561fa
Yuudachi_CivilEng <adsl2328606@gmail.com>
2019-07-21 02:54:13 +0800

    Create Week1-004:打印字符.cpp

---
+++ CPP Programming-PKU MOOC/Week1-004:打印字符.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ *  PKU 程序设计与算法慕课（三） C++面向对象 第一周004
+ *  004:打印字符
+ *  描述
+ *  输入一个ASCII码，输出对应的字符。
+ *
+ *  输入
+ *  一个整数，即字符的ASCII码，保证存在对应的可见字符。
+ *
+ *  输出
+ *  一行，包含相应的字符。
+ */
+
+int main(){
+    int c;
+    cin >> c;
+    cout << (char)c;
+    return 0;
+}
