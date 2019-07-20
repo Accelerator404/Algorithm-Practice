fed1877a03396aa328cfd49634afa4f26ba33b05
Yuudachi_CivilEng <adsl2328606@gmail.com>
2019-07-21 02:50:12 +0800

    Create Week1-002:字符菱形.cpp

---
+++ CPP Programming-PKU MOOC/Week1-002:字符菱形.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ *  PKU 程序设计与算法慕课（三） C++面向对象 第一周002
+ *  002:字符菱形
+ *  描述
+ *  给定一个字符，用它构造一个对角线长5个字符，倾斜放置的菱形。
+ *
+ *  输入
+ *  输入只有一行， 包含一个字符。
+ *
+ *  输出
+ *  该字符构成的菱形。
+ */
+
+int main(){
+    char c;
+    cin >> c;
+    for(int i = 1;i <= 25;i++){
+        if(i==3 || i==23 || (i>=7&&i<=19&&i!=10&&i!=16))
+            printf("%c",c);
+        else
+            printf(" ");
+        if(i%5==0)
+            printf("\n");
+    }
+    return 0;
+}
