57545b8c3acd38b6feb1230814b21c683af5a2b3
Accelerator404 <adsl2328606@gmail.com>
2019-07-25 17:48:18 +0800

    Update 5.h

--- Leetcode_CPP/5.h
+++ Leetcode_CPP/5.h
@@ -5,37 +5,44 @@ using namespace std;
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if(s.length() <= 1)
+        if(s.empty())
+            return "";
+        if(s.length() == 1)
             return s;
         string LPS;
-        str_plot u;
+        str_plot u,LPS_plot;
+        LPS_plot.pivot = 0;
+        LPS_plot.length = 1;
         vector<str_plot> PS_pool;
-        //先筛选出所有长度为2、3的回文串
-        for (int i = 0; i < s.length()-1; ++i) {
+        //先筛选出所有长度为2、3的回文串，严格保持顺序
+        for (int i = 0; i < s.length()-2; ++i) {
             u.pivot = i;
             u.length = 2;
             if(s[i]==s[i+1])
                 PS_pool.push_back(u);
-        }
-        for (int i = 0; i < s.length()-2; ++i) {
-            u.pivot = i;
             u.length = 3;
             if(s[i]==s[i+2])
                 PS_pool.push_back(u);
+        }
+        if(s[s.length()-2] == s[s.length()-1]){
+            u.pivot = s.length()-2;
+            u.length = 2;
+            PS_pool.push_back(u);
         }
         //不存在回文串则返回首字符
         if(PS_pool.empty())
             return s.substr(0,1);
         //对每个短回文串，向两侧同时延长，直到延长后的子串不是回文串
         for (auto it = PS_pool.begin(); it != PS_pool.end(); ++it) {
+            //当剩余未延长子串的最大长度小于等于目前的最长回文串长度时，跳出循环
+            if(s.length() - it->pivot - it->length/2 <= LPS_plot.length/2)
+                break;
             ExtendPS(s,*it);
-        }
-        //选出最长的回文串
-        for (auto it = PS_pool.begin(); it != PS_pool.end(); ++it) {
-            if(it->length > LPS.length()){
-                LPS = s.substr(it->pivot,it->length);
+            if(it->length > LPS_plot.length){
+                LPS_plot = *it;
             }
         }
+        LPS = s.substr(LPS_plot.pivot,LPS_plot.length);
         return LPS;
     }
     //用开始位置&长度表示一个子字符串
